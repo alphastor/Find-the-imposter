090f0ea6ac4b1e3cf6740958b500d8f76b52d560
alphastor <ulliprajwal@gmail.com>
2021-04-04 19:56:54 +0530

    imposter png added

--- main.py.py
+++ main.py.py
@@ -9,9 +9,15 @@ pygame.init()
 win = pygame.display.set_mode((600, 500))
 hold = True
 
+#--------------platform-----------------------#
 platform = pygame.image.load("platform_1.png")
 def draw_platform():
     win.blit(platform, (1,20))
+
+#---------------imposter---------------------#
+ball = pygame.image.load("ball_1.png")
+def draw_ball():
+    win.blit(ball, (300,250))
 
 
 #--------------------------#--------------------------#--------------------------#--------------------------#
@@ -22,6 +28,7 @@ while hold:
 
     win.fill((0, 0, 0))
     draw_platform()
+    draw_ball()
 
     #--------------------------#--------------------------#--------------------------#--------------------------#
     pygame.display.update()
