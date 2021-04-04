239f7a7ec5ccf77b6ea628750a112e115579fc8e
alphastor <ulliprajwal@gmail.com>
2021-04-04 19:38:16 +0530

    __init__

---
+++ main.py.py
@@ -0,0 +1,27 @@
+import pygame
+from random import randint
+from os import system
+
+system('cls')
+
+pygame.init()
+
+win = pygame.display.set_mode((600, 500))
+hold = True
+
+platform = pygame.image.load("platform_2.png")
+def draw_platform():
+    win.blit(platform, (1,20))
+
+
+#--------------------------#--------------------------#--------------------------#--------------------------#
+while hold:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            hold = False
+
+    win.fill((0, 0, 0))
+    draw_platform()
+
+    #--------------------------#--------------------------#--------------------------#--------------------------#
+    pygame.display.update()
